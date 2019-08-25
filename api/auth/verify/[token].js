488ad2198b49587db4235c937de7bcd3140bd954
Tomas Fagerbekk <tomas@otovo.com>
2019-08-25 17:46:20 +0200

    auth: verify GENERATE_AUTH_TOKEN permission in JWT

--- api/auth/verify/[token].js
+++ api/auth/verify/[token].js
@@ -1,5 +1,6 @@
 import {JsonWebTokenError, TokenExpiredError} from 'jsonwebtoken';
 import auth from '../../../auth';
+import * as permissions from "../../../permissions";
 
 module.exports = (req, res) => {
   const { token } = req.query;
@@ -9,15 +10,19 @@ module.exports = (req, res) => {
     data = auth.verify(token);
   } catch (err) {
     if (err instanceof TokenExpiredError) {
-      return res.status(401).send();
+      return res.status(401).json({status: 401, message: 'Token has expired.'});
     }
     if (err instanceof JsonWebTokenError) {
-      return res.status(400).send('Invalid JWT format.');
+      return res.status(400).json({status: 400, message: 'Invalid JWT format.'});
     }
     console.log(err);
-    return res.status(500).send('Error');
+    return res.status(500).json({status: 500, message: 'Internal server error'});
   }
-  const newToken = auth.generateDataToken(data.id);
 
+  if (data.permissions.indexOf(permissions.GENERATE_AUTH_TOKEN) === -1) {
+    return res.status(403).json({status: 403, message: `This token is not authorized for ${permissions.GENERATE_AUTH_TOKEN}`})
+  }
+
+  const newToken = auth.generateDataToken(data.id);
   return res.status(200).json({ token: newToken });
 };
